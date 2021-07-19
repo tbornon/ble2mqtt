1dbb0178b4b1038ae3f98d2dba039442e46fa492
Théophile BORNON <theophile.bornon@protonmail.com>
2021-07-19 09:46:24 +0100

    remove lib folder

--- lib/helpers/Converters.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-export declare function getTemperature(data: Buffer): number;
-export declare function getHumidity(data: Buffer): number;
-//# sourceMappingURL=Converters.d.ts.map
